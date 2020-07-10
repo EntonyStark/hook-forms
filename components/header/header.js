da69837221e6c8f31e77645fbfebb634a6c574cf
Artem Stepanov <artem.s@twsgo.com>
2020-07-10 11:26:54 +0300

    master update main page

--- components/header/header.js
+++ components/header/header.js
@@ -24,47 +24,49 @@ export const Header = ({ repoInfo }) => {
   const router = useRouter();
 
   return (
-    <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-light">
-      <div className="container">
-        <Link href={URLS.home}>
-          <a className="navbar-brand">{`${packageTitle}, v${getPackageVersion()}`}</a>
-        </Link>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarContent"
-          aria-controls="navbarContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
+    <header className="sticky-top">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link href={URLS.home}>
+            <a className="navbar-brand">{`${packageTitle}, v${getPackageVersion()}`}</a>
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarContent"
+            aria-controls="navbarContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
 
-        <div className="collapse navbar-collapse" id="navbarContent">
-          <ul className="navbar-nav ml-auto mr-2">
-            {navItems.map((el) => (
-              <li key={el.id} className={el.link === router.pathname ? 'nav-item active' : 'nav-item'}>
-                <Link href={el.link}>
-                  <a className="nav-link">{el.title}</a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-          <form className="form-inline" style={{ width: '10%' }}>
-            <div className="input-group input-group-sm">
-              <div className="input-group-prepend">
-                <span className="input-group-text" id="basic-addon1"><i className="fab fa-github" /></span>
+          <div className="collapse navbar-collapse" id="navbarContent">
+            <ul className="navbar-nav ml-auto mr-2">
+              {navItems.map((el) => (
+                <li key={el.id} className={el.link === router.pathname ? 'nav-item active' : 'nav-item'}>
+                  <Link href={el.link}>
+                    <a className="nav-link">{el.title}</a>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <form className="form-inline" style={{ width: '10%' }}>
+              <div className="input-group input-group-sm">
+                <div className="input-group-prepend">
+                  <span className="input-group-text" id="basic-addon1"><i className="fab fa-github" /></span>
+                </div>
+                <input
+                  value={repoInfo.stargazers_count || ''}
+                  className="form-control bg-white"
+                  disabled
+                />
               </div>
-              <input
-                value={repoInfo.stargazers_count || ''}
-                className="form-control bg-white"
-                disabled
-              />
-            </div>
-          </form>
+            </form>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </header>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -7,60 +7,80 @@ export const getStaticProps = async () => ({
   props: {},
 });
 
-const Home = ({ repoInfo }) => {
-  console.log('props', repoInfo);
-  return (
-    <>
-      <div className="row justify-content-center my-5">
-        <div className="col-auto py-5">
-          <h1 className="display-3">
-            Welcome to <a href={repoInfo.html_url}>{packageTitle}</a>
-          </h1>
-          <hr className="my-4" />
-          <p className="lead">The best way to manage your form state with hooks.</p>
+const Home = ({ repoInfo }) => (
+  <>
+    <div className="row justify-content-center mt-5">
+      <div className="col-auto py-5">
+        <h1 className="display-3">
+          Welcome to <a href={repoInfo.html_url}>{packageTitle}</a>
+        </h1>
+        <hr className="my-4" />
+        <p className="lead">The best way to manage your form state with hooks.</p>
+      </div>
+    </div>
+    <div className="row justify-content-center mb-5">
+      <h3>Features</h3>
+    </div>
+    <div className="row my-5">
+      <div className="col-sm-12 col-md-4">
+        <article className="text-center">
+          <h3>DX</h3>
+          <p>Intuitive, feature-complete API providing a seamless experience to developers when building forms.</p>
+        </article>
+      </div>
+      <div className="col-sm-12 col-md-4">
+        <article className="text-center">
+          <h3>Super Light</h3>
+          <p>Package size matters. React Hook Form is a tiny library without any dependencies.</p>
+        </article>
+      </div>
+      <div className="col-sm-12 col-md-4">
+        <article className="text-center">
+          <h3>Adoptable</h3>
+          <p>Since form state is inherently local, it can be easily adopted without other dependencies.</p>
+        </article>
+      </div>
+    </div>
+    <div className="row justify-content-center my-5">
+      <div className="col-sm-4">
+        <div className="card mb-4">
+          <div className="card-body">
+            <p className="card-text">Start learning new things here</p>
+            <Link href={URLS.getStarted}>
+              <a className="btn btn-primary">Getting started &rarr;</a>
+            </Link>
+          </div>
+        </div>
+        <div className="card">
+          <div className="card-body">
+            <p className="card-text">See how you can use it</p>
+            <Link href={URLS.examples.simple}>
+              <a className="btn btn-primary">Examples &rarr;</a>
+            </Link>
+          </div>
         </div>
       </div>
-      <div className="row justify-content-center my-5">
-        <div className="col-sm-4">
-          <div className="card mb-4">
-            <div className="card-body">
-              <p className="card-text">Start learning new things here</p>
-              <Link href={URLS.getStarted}>
-                <a className="btn btn-primary">Getting started &rarr;</a>
-              </Link>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <p className="card-text">See how you can use it</p>
-              <Link href={URLS.examples.simple}>
-                <a className="btn btn-primary">Examples &rarr;</a>
-              </Link>
-            </div>
+      <div className="col-sm-4">
+        <div className="card mb-4">
+          <div className="card-body">
+            <p className="card-text">Description for API</p>
+            <Link href={URLS.doc.api}>
+              <a className="btn btn-primary">Documentation &rarr;</a>
+            </Link>
           </div>
         </div>
-        <div className="col-sm-4">
-          <div className="card mb-4">
-            <div className="card-body">
-              <p className="card-text">Description for API</p>
-              <Link href={URLS.doc.api}>
-                <a className="btn btn-primary">Documentation &rarr;</a>
-              </Link>
-            </div>
-          </div>
-          <div className="card">
-            <div className="card-body">
-              <p className="card-text">Create your form array with us</p>
-              <Link href={URLS.doc.api}>
-                <a className="btn btn-primary">Form constructor &rarr;</a>
-              </Link>
-            </div>
+        <div className="card">
+          <div className="card-body">
+            <p className="card-text">Create your form array with us</p>
+            <Link href={URLS.doc.api}>
+              <a className="btn btn-primary">Form constructor &rarr;</a>
+            </Link>
           </div>
         </div>
       </div>
-    </>
-  );
-};
+    </div>
+  </>
+);
 
 export default () => (
   <BaseLayout>
