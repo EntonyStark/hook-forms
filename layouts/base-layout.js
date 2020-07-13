7184beb6d3101010a11cac3fa501338923ea17a3
Artem Stepanov <artem.s@twsgo.com>
2020-07-13 12:27:55 +0300

    master add new favicon

--- layouts/base-layout.js
+++ layouts/base-layout.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Head from 'next/head';
 
 import { getRepoInfo } from '../actions/github';
 
@@ -15,19 +14,11 @@ export const BaseLayout = ({ children }) => {
 
   return (
     <>
-      <Head>
-        <title>Hook Easy Form</title>
-        <link rel="icon" href="/favicon.ico" />
-        <meta itemProp="name" content="Hook Easy Form" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="Hook Easy Form" />
-        <meta property="og:site_name" content="Hook Easy Form" />
-        <meta property="og:description" content="The best way to manage your form state with hooks." />
-        <meta property="og:image" content="vercel.svg" />
-        {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha512-xA6Hp6oezhjd6LiLZynuukm80f8BoZ3OpcEYaqKoCV3HKQDrYjDE1Gu8ocxgxoXmwmSzM4iqPvCsOkQNiu41GA==" crossOrigin="anonymous" /> */}
-        {/* <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossOrigin="anonymous" /> */}
-        {/* <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous" /> */}
-      </Head>
+      {/* <Head> */}
+      {/* <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.1/css/all.min.css" integrity="sha512-xA6Hp6oezhjd6LiLZynuukm80f8BoZ3OpcEYaqKoCV3HKQDrYjDE1Gu8ocxgxoXmwmSzM4iqPvCsOkQNiu41GA==" crossOrigin="anonymous" /> */}
+      {/* <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossOrigin="anonymous" /> */}
+      {/* <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossOrigin="anonymous" /> */}
+      {/* </Head> */}
       <Header repoInfo={repoInfo} />
       <div className="container">
         {React.cloneElement(children, { repoInfo })}
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,26 @@
+import Head from 'next/head';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import '../styles/index.scss';
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Hook Easy Form</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+
+        <meta itemProp="name" content="Hook Easy Form" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Hook Easy Form" />
+        <meta property="og:site_name" content="Hook Easy Form" />
+        <meta property="og:description" content="The best way to manage your form state with hooks." />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
